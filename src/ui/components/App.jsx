2a1ebb05405463ce67057785d6b3664043b8547d
George Maddocks <gmaddocks1@gmail.com>
2018-05-20 17:43:55 +0100

    Use decorators; redux dev tools; slightly better markup

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -1,35 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { autobind } from 'core-decorators';
 import * as gameActions from '../modules/game';
 
+class ScoreButton extends Component {
+  @autobind
+  _onClick() {
+    this.props.onClick(this.props.playerNumber);
+  }
+  render() {
+    return (
+      <button onClick={this._onClick}>
+        Player {this.props.playerNumber}
+      </button>);
+  }
+}
+class App extends Component {
+  @autobind
+  score(playerNumber) {
+    this.props.incrementPoints(`player${playerNumber}`);
+  }
 
-const App = ({ game, set, incrementPlayer1Points, incrementPlayer2Points }) => (
-  <div>
-    <div>
+  render() {
+    return (
       <div>
-        <span>Game</span>
-        <span>Player 1: {game.player1Points}</span>
-        <span>Player 2: {game.player2Points}</span>
-        <span>CurrentSet</span>
-        <span>Player 1: {set.player1Games}</span>
-        <span>Player 2: {set.player2Games}</span>
-      </div>
-      <button onClick={incrementPlayer1Points}>Player 1</button>
-      <button onClick={incrementPlayer2Points}>Player 2</button>
-    </div>
-  </div>);
+        <div>
+          <div>
+            <div>
+              <h4>Game</h4>
+              <div>Player 1: {this.props.game.player1Points}</div>
+              <div>Player 2: {this.props.game.player2Points}</div>
+            </div>
+            <div>
+              <h4>Current Set</h4>
+              <div>Player 1: {this.props.set.player1Games}</div>
+              <div>Player 2: {this.props.set.player2Games}</div>
+            </div>
+          </div>
+          <ScoreButton playerNumber={1} onClick={this.score} />
+          <ScoreButton playerNumber={2} onClick={this.score} />
+        </div>
+      </div>);
+  }
+}
 
 App.propTypes = {
   game: PropTypes.object.isRequired,
   set: PropTypes.object.isRequired,
-  incrementPlayer1Points: PropTypes.func.isRequired,
-  incrementPlayer2Points: PropTypes.func.isRequired,
+  incrementPoints: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = {
-  incrementPlayer2Points: gameActions.incrementPlayer2Points,
-  incrementPlayer1Points: gameActions.incrementPlayer1Points,
+  incrementPoints: gameActions.incrementPoints,
 };
 
 export default connect(
--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -6,7 +6,11 @@ import reducer from './modules';
 import App from './components/App';
 
 const mountElement = document.querySelector('#mountpoint');
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  undefined,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 const provider = <Provider store={store}><App /></Provider>;
 ReactDOM.render(provider, mountElement);
